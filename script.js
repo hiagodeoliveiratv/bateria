ee6154aef69cba917e1789303f256aa7c8c270f3
Hiago de Oliveira <hiagodeoliveiratv@gmail.com>
2021-09-01 11:00:59 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ document.body.addEventListener("keyup", (event)=>{
     playSound(event.code.toLowerCase());
 });
 
+document.querySelector(".composer button").addEventListener('click', ()=>{
+    let song = document.querySelector("#input").value;
+
+    if(song !== ""){
+        let songArray = song.split('');
+        playComposition(songArray);
+    }
+});
+
 function playSound(sound){
     let audioElement = document.querySelector(`#s_${sound}`);
     let keyElement = document.querySelector(`div[data-key="${sound}"]`);
@@ -16,5 +25,17 @@ function playSound(sound){
        setTimeout(()=>{
         keyElement.classList.remove("active");
        }, 100);
+    }
+}
+
+function playComposition(songArray){
+
+    let wait = 0;
+
+    for(let songItem of songArray){
+        setTimeout(()=>{
+            playSound(`key${songItem}`);
+        }, wait);
+        wait += 250;
     }
 }
